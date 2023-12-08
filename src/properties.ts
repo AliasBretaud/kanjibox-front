0a2827b3002df999704d2fd0367a803a839e8a5c
Florian Bretaud <lcdopg@gmail.com>
2023-12-08 18:12:29 +0900

    update dependencies

--- src/properties.ts
+++ src/properties.ts
@@ -1,6 +1,6 @@
 export const properties = {
   kanjiApi: {
     //url: "https://kanjibox.appspot.com",
-    url: "http://192.168.1.107:8080/kanjibox",
+    url: "http://localhost:8080/kanjibox",
   },
 };
