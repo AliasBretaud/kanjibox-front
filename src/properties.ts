ea718e5800714bd143b9aa55b366e2a3b1865491
Florian Bretaud <lcdopg@gmail.com>
2023-12-12 18:11:05 +0900

    Update properties.ts

--- src/properties.ts
+++ src/properties.ts
@@ -1,6 +1,6 @@
 export const properties = {
   kanjiApi: {
-    url: "https://kanjibox-api-1d5274f00f7b.herokuapp.com",
+    url: "https://kanjibox-api-1d5274f00f7b.herokuapp.com/kanjibox",
     //url: "http://localhost:8080/kanjibox",
   },
 };
