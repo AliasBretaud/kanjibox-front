21af295fe11a1e970b390ee96b84a3f27a8a3396
Florian Bretaud <lcdopg@gmail.com>
2024-04-01 11:51:38 +0900

    small updates (#3)
    * small updates
    
    * fix loading padding

--- src/components/ui/LoadingState.tsx
+++ src/components/ui/LoadingState.tsx
@@ -1,6 +1,8 @@
+import { CircularProgress } from "@mui/material";
+
 export const LoadingState = ({ text }: { text: string }) => (
-  <div className="w-full flex flex-col items-center p-6 gap-2">
+  <div className="w-full flex flex-col items-center p-6 gap-4">
     <p>{text}</p>
-    <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    <CircularProgress size={80} />
   </div>
 );
