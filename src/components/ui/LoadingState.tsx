3a2fb50a1ee0f0cb64d1753a04aca62d388e99fd
Florian Bretaud <lcdopg@gmail.com>
2024-04-01 11:50:02 +0900

    fix loading padding

--- src/components/ui/LoadingState.tsx
+++ src/components/ui/LoadingState.tsx
@@ -1,19 +1,8 @@
-import { CircularProgress, Container } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 
 export const LoadingState = ({ text }: { text: string }) => (
-  <Container
-    maxWidth={false}
-    sx={{
-      height: "calc(100vh - 64px)",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      paddingBottom: "100px",
-    }}
-  >
-    <div className="flex flex-col items-center gap-4">
-      <p>{text}</p>
-      <CircularProgress size={80} />
-    </div>
-  </Container>
+  <div className="w-full flex flex-col items-center p-6 gap-4">
+    <p>{text}</p>
+    <CircularProgress size={80} />
+  </div>
 );
